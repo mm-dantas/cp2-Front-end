4af8832258cb6cc9ec105c51c2d8a07a6ef4b622
Matheus Dantas <161778495+mm-dantas@users.noreply.github.com>
2024-05-01 12:03:27 -0300

    Add files via upload

---
+++ js/indexScript.js
@@ -0,0 +1,11 @@
+function comprar1(){
+    window.location.href = "./Freixenet Pinot Grigio.html"
+}
+
+function comprar2(){
+    window.location.href = "./Luiz Argenta Jovem Red Audrey.html"
+}
+
+function comprar3(){
+    window.location.href = "./Manus Liberum Barbera Clairet Rosé.html"
+}
